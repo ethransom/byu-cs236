83104fac2a782c939d9362a659fef04d1d03b023
Ethan Ransom <derethanhausen@gmail.com>
2014-12-11 00:05:48 -0700

    Optimize ALL the things
    Conflicts:
    	project_1/datalogprogram.cpp
    	project_1/datalogprogram.h

--- project_1/scanner.cpp
+++ project_1/scanner.cpp
@@ -5,15 +5,15 @@
 
 static const std::string ALPHA_NUMERIC = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
 
+uint scanner_pos = 0;
+
 bool read_until(std::string* input, std::string delimiter, std::string* output) {
-	size_t pos = 0;
+	size_t pos = input->find(delimiter, scanner_pos);
 	std::string token;
-	pos = input->find(delimiter);
 
 	if (pos != std::string::npos) {
-		token = input->substr(0, pos);
-		input->erase(0, pos+delimiter.length());
-		*output = token;
+		*output = input->substr(scanner_pos, (pos - scanner_pos));
+		scanner_pos = pos + delimiter.length();
 		return true;
 	} else {
 		return false;
@@ -21,13 +21,11 @@ bool read_until(std::string* input, std::string delimiter, std::string* output) 
 }
 
 bool read_string(std::string* input, std::string value, std::string* output) {
-	size_t pos = 0;
-	std::string token;
-	pos = input->find(value);
+	std::string token = input->substr(scanner_pos, value.length());
 
-	if (pos == 0) { // input must start with desired string
-		token = input->substr(0, value.length());
-		input->erase(0, value.length());
+	if (token == value) { // input must start with desired string
+		// input->erase(0, value.length());
+		scanner_pos += value.length();
 		*output = token;
 		return true;
 	} else {
@@ -46,10 +44,11 @@ bool read_string(std::string* input, std::string value, std::string* output) {
 // can easily understand that verbosity and simplicity are not mutually exclusive.
 
 bool read_identifier(std::string* input, std::string* output, Token_type* type) {
-	if (isalpha(input->at(0))) {
-		size_t pos = input->find_first_not_of(ALPHA_NUMERIC);
-		*output = input->substr(0, pos);
-		input->erase(0, pos);
+	if (isalpha(input->at(scanner_pos))) {
+		size_t pos = input->find_first_not_of(ALPHA_NUMERIC, scanner_pos);
+		*output = input->substr(scanner_pos, (pos - scanner_pos));
+		// input->erase(0, pos);
+		scanner_pos = pos;
 
 		if (*output == "Schemes") *type = SCHEMES;
 		else if (*output == "Facts") *type = FACTS;
@@ -98,7 +97,7 @@ bool read_punctuation(std::string* input, std::string* output, Token_type* type)
 #define SKIP_WHITESPACE if ( read_string(&str, "\t", &output) || read_string(&str, " ", &output)) continue;
 
 // it's actually about ethics in game journalism!
-#define MAIN_LOOP while (str.length() != 0)
+#define MAIN_LOOP while ((scanner_pos + 1) < str.length())
 
 // Fills the given vector with tokens read from the given string.
 // Exits early on error, returning the line number of the error.
@@ -131,7 +130,9 @@ int Scanner::lex_file(std::string str, std::vector<Token>** token_vec) {
 				// string ended, check for lines inside string
 
 				size_t pos = (&output)->find("\n");
-				if (pos != std::string::npos) return line;
+				if (pos != std::string::npos) {
+					return line;
+				}
 			} else {
 				// string did not end
 				return line;
